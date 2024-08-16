f5a5f30bc168d6d1a18429ec6ddb6cbe532c051d
erickfreitas <erickfreitasbezerra@gmail.com>
2024-08-16 16:55:22 -0300

    estilizando Views

--- first-app/App.js
+++ first-app/App.js
@@ -1,28 +1,21 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.customText}>Another Text!</Text>
-      <Text style={styles.customText}>Hello World!</Text>
-      <Button title='Press me' onPress={() => alert('Button pressed')} />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Enter text'></TextInput>
+        <Button title='Add Goal'></Button>
+      </View>
+      <View>
+        <Text>List of goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  customText: {
-    fontSize: 30,
-    color: 'orange',
-    padding: 10,
-    margin: 10,
-    borderWidth: 1,
-    borderColor: 'orange',
+  appContainer: {
+    padding: 50,
   },
 });
