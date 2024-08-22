bdcfa3ee1ce2953550e7a4d61dfe704d892dfa7a
erickfreitas <erickfreitasbezerra@gmail.com>
2024-08-22 20:49:09 -0300

    utilizando recurso Alert

--- guess-number-app/components/PrimaryButton.js
+++ guess-number-app/components/PrimaryButton.js
@@ -1,10 +1,10 @@
 import { View, Text, Pressable, StyleSheet } from 'react-native';
 
-function PrimaryButton({ children }) {
+function PrimaryButton({ children, onPress }) {
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
-        onPress={() => {}}
+        onPress={onPress}
         style={({ pressed }) =>
           pressed
             ? [styles.buttonInnerContainer, styles.pressed]
