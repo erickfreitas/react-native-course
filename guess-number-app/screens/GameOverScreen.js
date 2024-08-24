32621dd1320be14dd6606a9b2e686a046d994b8d
erickfreitas <erickfreitasbezerra@gmail.com>
2024-08-24 20:24:35 -0300

    adicionando imagem de fundo

--- guess-number-app/screens/GameOverScreen.js
+++ guess-number-app/screens/GameOverScreen.js
@@ -1,7 +1,45 @@
-import { View, Text } from 'react-native';
+import { View, Image, StyleSheet, Text } from 'react-native';
+
+import Title from '../components/ui/Title';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 function GameOver() {
-  return <Text>Game Over</Text>;
+  return (
+    <View style={styles.screen}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/success.png')}
+        />
+      </View>
+      <View>
+        <Text>Your phone needed X rounds to guess the number Y.</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default GameOver;
