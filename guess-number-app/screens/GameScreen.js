57f0d02f301e8917fb37fca3bd92f6715b82b710
erickfreitas <erickfreitasbezerra@gmail.com>
2024-08-23 21:28:22 -0300

    utilizando contantes para gerenciar as cores do app

--- guess-number-app/screens/GameScreen.js
+++ guess-number-app/screens/GameScreen.js
@@ -1,9 +1,11 @@
 import { View, Text, StyleSheet } from 'react-native';
 
+import Title from '../components/Title';
+
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text style={styles.title}>Opponent's Guess</Text>
+      <Title>Opponent's Guess</Title>
       {/* GUESS */}
       <View>
         <Text>Higher or Lower?</Text>
@@ -18,15 +20,6 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#ddb52f',
-    textAlign: 'center',
-    borderWidth: 2,
-    borderColor: '#ddb52f',
-    padding: 12,
   },
 });
 
