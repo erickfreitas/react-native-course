09052c4642068bc0e51d192ec2d400e09e9f9de7
erickfreitas <erickfreitasbezerra@gmail.com>
2024-08-27 19:44:00 -0300

    implementado exibição do componente MealItem

--- navigation-meals-app/screens/MealsOverviewScreen.js
+++ navigation-meals-app/screens/MealsOverviewScreen.js
@@ -1,26 +1,40 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 //import { useRoute } from '@react-navigation/native';
 
+import MealItem from '../components/MealItem';
 import { MEALS } from '../data/dummy-data';
 
 function MealsOverviewScreen({ route }) {
-
   //Também é possível usar o useRoute para obter os parâmetros da rota.
   //const route = useRoute();
   //route.params;
 
   const categoryId = route.params.categoryId;
 
-  return <View style={styles.container}>
-    <Text>MealsOverviewScreen - {categoryId}</Text>
-  </View>
+  const displayedMeals = MEALS.filter((meal) =>
+    meal.categoryIds.includes(categoryId)
+  );
+
+  function renderMealItem(itemData) {
+    return <MealItem {...itemData.item} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16
-  }
-})
+    padding: 16,
+  },
+});
 
 export default MealsOverviewScreen;
