8b49cee1ce87cb92110ddf5e91f91b2e45276e72
erickfreitas <erickfreitasbezerra@gmail.com>
2024-08-28 22:44:45 -0300

    instalando dependências do React Navigation e criando um menu de navegação com Drawer Navigator

--- drawer-navigation-app/App.js
+++ drawer-navigation-app/App.js
@@ -1,5 +1,17 @@
-import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import UserScreen from './screens/UserScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 
 export default function App() {
-  return <Text>Todo...</Text>;
+
+  const Drawer = createDrawerNavigator();
+
+  return <NavigationContainer>
+    <Drawer.Navigator initialRouteName="Welcome">
+      <Drawer.Screen name="User" component={UserScreen} />
+      <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+    </Drawer.Navigator>
+  </NavigationContainer>;
 }
