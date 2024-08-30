69e4eab598cfa9cac57015d610385a4ac961baa1
erickfreitas <erickfreitasbezerra@gmail.com>
2024-08-29 21:03:49 -0300

    configurando e estilizando a a aparência do menu lateral Drawer Navigator

--- drawer-navigation-app/App.js
+++ drawer-navigation-app/App.js
@@ -1,17 +1,53 @@
+import { Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
+import { StatusBar } from 'expo-status-bar';
 
 import UserScreen from './screens/UserScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 
 export default function App() {
-
   const Drawer = createDrawerNavigator();
 
-  return <NavigationContainer>
-    <Drawer.Navigator initialRouteName="Welcome">
-      <Drawer.Screen name="User" component={UserScreen} />
-      <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-    </Drawer.Navigator>
-  </NavigationContainer>;
+  return (
+    <>
+      <StatusBar style='light' />
+      <NavigationContainer>
+        <Drawer.Navigator
+          initialRouteName='Welcome'
+          screenOptions={{
+            headerStyle: { backgroundColor: '#3c0a6b' },
+            headerTintColor: '#fff',
+            drawerActiveBackgroundColor: '#2c0451',
+            drawerActiveTintColor: '#fff',
+            drawerInactiveTintColor: '#ffffff',
+            drawerContentStyle: {
+              backgroundColor: '#3c0a6b',
+            },
+          }}
+        >
+          <Drawer.Screen
+            name='Welcome'
+            component={WelcomeScreen}
+            options={{
+              drawerLabel: 'Welcome Screen',
+              drawerIcon: ({ color, size }) => {
+                return <Ionicons name='home' color={color} size={size} />;
+              },
+            }}
+          />
+          <Drawer.Screen
+            name='User'
+            component={UserScreen}
+            options={{
+              drawerIcon: ({ color, size }) => {
+                return <FontAwesome5 name='users' color={color} size={size} />;
+              },
+            }}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
