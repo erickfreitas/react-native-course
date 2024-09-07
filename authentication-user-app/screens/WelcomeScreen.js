7f93d10996054eb9595db97727137167eba901f1
erickfreitas <erickfreitasbezerra@gmail.com>
2024-09-06 21:06:38 -0300

    fazendo requisição na api do firebase passando o token de autenticação

--- authentication-user-app/screens/WelcomeScreen.js
+++ authentication-user-app/screens/WelcomeScreen.js
@@ -1,10 +1,28 @@
+import { useEffect, useState, useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const authCtx = useContext(AuthContext);
+  const [message, setMessage] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://react-native-course-af1d3-default-rtdb.firebaseio.com/message.json?auth=${authCtx.token}`
+      )
+      .then((response) => {
+        setMessage(response.data);
+      });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
--- authentication-user-app/store/auth-context.js
+++ authentication-user-app/store/auth-context.js
@@ -1,5 +1,4 @@
 import { createContext, useState } from "react";
-import { authenticate } from "../util/auth";
 
 export const AuthContext = createContext({
   token: '',
