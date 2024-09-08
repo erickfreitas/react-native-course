d5e6e82db24c0f8b337b361ad21d0a8993d094b6
erickfreitas <erickfreitasbezerra@gmail.com>
2024-09-08 18:18:23 -0300

    criando a navegação entre a tela de listagem de lugares e a tela de cadastro de lugares

--- favorite-place-app/App.js
+++ favorite-place-app/App.js
@@ -1,19 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import ProductList from './screens/ProductList';
+import AllPlaces from './screens/AllPlaces';
+import AddPlaces from './screens/AddPlaces';
+import IconButtons from './ui/IconButtons';
+
+import { Colors } from './constants/colors';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    // <>
-    //   <StatusBar style='auto' />
-    //   <ProductList />
-    // </>
-
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: Colors.primary500,
+            },
+            headerTintColor: Colors.gray700,
+            contentStyle: {
+              backgroundColor: Colors.gray700,
+            },
+          }}
+        >
+          <Stack.Screen
+            name='AllPlaces'
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: 'Your Favorite Places',
+              headerRight: (tintColor) => (
+                <IconButtons
+                  icon='add'
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate('AddPlaces')}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name='AddPlaces'
+            component={AddPlaces}
+            options={{
+              title: 'Add a new place',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
--- favorite-place-app/components/Places/PlacesList.js
+++ favorite-place-app/components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 import { FlatList, View, Text, StyleSheet } from 'react-native';
 
+import { Colors } from '../../constants/colors';
 import PlaceItem from './PlaceItem';
 
 function PlacesList({ places }) {
@@ -30,5 +31,6 @@ const styles = StyleSheet.create({
   },
   fallbackText: {
     fontSize: 16,
+    color: Colors.primary200,
   },
 });
