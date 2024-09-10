014f0beee578afccf017b238373b226dee4c0b56
erickfreitas <erickfreitasbezerra@gmail.com>
2024-09-09 21:33:04 -0300

    criando componente que utiliza a biblioteca de geolocalização do expo

--- favorite-place-app/components/Places/LocationPicker.js
+++ favorite-place-app/components/Places/LocationPicker.js
@@ -1,5 +1,83 @@
+import { View, StyleSheet, Alert } from 'react-native';
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from 'expo-location';
+
+import OutlinedButton from '../ui/OutlinedButton';
+import { Colors } from '../../constants/colors';
+
 function LocationPicker() {
-  return ;
+  const [locationPermissionInfo, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (locationPermissionInfo.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInfo.status == PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient permissions!',
+        'You need to grant location permissions to use this app.'
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (hasPermission === false) {
+      return;
+    }
+    
+    const location = await getCurrentPositionAsync({
+      accuracy: 5,
+      enableHighAccuracy: true,
+    });
+
+    console.log(location);
+  }
+
+  function pickOnMapHandler() {}
+
+  return (
+    <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutlinedButton icon='location' size={24} onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon='map' size={24} onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
+      </View>
+    </View>
+  );
 }
 
 export default LocationPicker;
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+    overflow: 'hidden',
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+});
