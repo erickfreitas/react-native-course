af94c03936b7bfb8ae24e2ba1943d2fdaf53730e
erickfreitas <erickfreitasbezerra@gmail.com>
2024-09-16 19:34:42 -0300

    removendo api key do git (obs: key foi apagada)

--- favorite-place-app/util/location.js
+++ favorite-place-app/util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyBKSVYS6YMZB4LKaCqcgLtMh40DMNPnzn4';
+const GOOGLE_API_KEY = 'XXXXXXXXXXXXXXXXXXX';
 
 export function getMapPreviewUrl(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
